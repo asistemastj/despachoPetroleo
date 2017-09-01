47e92789a30d52ff156abd33ec4190e6bdc5764c
asistemas <asistemas@transportesjoselito.com>
2017-09-01 10:18:45 -0500

    controladores para mostrar despacho de las 2 empresas

---
+++ app/Http/Controllers/DespachoTjoselitoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PedidoAlmacenDetalle;
+use Illuminate\Http\Request;
+
+class DespachoTjoselitoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PedidoAlmacenDetalle  $pedidoAlmacenDetalle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PedidoAlmacenDetalle $pedidoAlmacenDetalle)
+    {
+        //
+    }
+}
